007cb466c2ea1cedce31888c41bd5177aef2723c
Alberto Elias <me@alberto-elias.com>
2015-10-25 19:57:18 +0000

    Week 1 in go

---
+++ week1/notes.md
@@ -0,0 +1,44 @@
+Machine learning: "A computer is said to learn from experience E with respect to some class of tasks T and performance measure P, if its performance at tasks in T, as measured by P, improves with experience E."
+
+# Supervised learning
+
+Relationship between input and output
+
+- Regression. Map input variables to some continuous function
+- Classification. Map input variables into discrete categories
+
+# Unsupervised learning
+
+Derive structure from data
+
+- Clustering. Puts together data based on relationships among the variables in the data.
+- Associative. Estamates mappings between variables.
+
+# Linear Regression with One Variable
+
+"univariate linear regression". Single ouput from single input.
+
+## Hypothesis Function
+
+hθ (x) = θ0 + θ1 * x
+
+## Cost Function
+
+Measures the accuracy of the hypothesis function taking the average of all the results of the hypothesis with all the inputs
+
+J(θ0, θ1) = (1 / 2 * m) * ∑(hθ(xi) - yi) ^ 2
+
+## Gradient Descent
+
+Improves the hypothesis function. It finds the local minimum in the graph of the cost function against θ0 and θ1.
+
+We take the derivative of the cost function, which gives us the direction to move towards at a learning rate α.
+
+θj := θj - α * derivative of J(θ0, θ1)
+
+Applied to θ0 and θ1:
+
+θ0 := θ0 - α * (1 / m) * ∑(hθ(xi) - yi)
+
+θ1 := θ1 - α * (1 / m) * ∑((hθ(xi) - yi) * xi)
+
---
+++ week1/regression.go
@@ -0,0 +1,64 @@
+package main
+
+import (
+	"fmt"
+	"math"
+)
+
+type Data struct {
+	x float64
+	y float64
+}
+
+func GenerateFakeData() []Data {
+	data := make([]Data, 3)
+	for i := 0; i < len(data); i++ {
+		data[i] = Data{
+			x: float64(i),
+			y: float64(i),
+		}
+	}
+
+	return data
+}
+
+func Hypothesis(theta0 float64, theta1 float64, x float64) float64 {
+	return theta0 + theta1*x
+}
+
+func GradientDescent(dataset []Data) (float64, float64) {
+	const alpha float64 = 0.05
+	var theta0 float64 = 0.0
+	var theta1 float64 = 0.0
+	var m float64 = float64(len(dataset))
+
+	for {
+		var theta0DataSum float64 = 0.0
+		for i := 0; i < len(dataset); i++ {
+			theta0Hypothesis := Hypothesis(theta0, theta1, dataset[i].x) - dataset[i].y
+			theta0DataSum = theta0DataSum + theta0Hypothesis
+		}
+		var temptheta0 float64 = theta0 - alpha*(1.0/m)*theta0DataSum
+
+		var theta1DataSum float64 = 0.0
+		for i := 0; i < len(dataset); i++ {
+			theta1Hypothesis := (Hypothesis(theta0, theta1, dataset[i].x) - dataset[i].y) * dataset[i].x
+			theta1DataSum = theta1DataSum + theta1Hypothesis
+		}
+		var temptheta1 float64 = theta1 - alpha*(1.0/m)*theta1DataSum
+
+		if math.Abs(theta0-temptheta0) <= 0.000001 || math.Abs(theta1-temptheta1) <= 0.000001 {
+			break
+		}
+
+		theta0 = temptheta0
+		theta1 = temptheta1
+	}
+
+	return theta0, theta1
+}
+
+func main() {
+	theta0, theta1 := GradientDescent(GenerateFakeData())
+	fmt.Println(fmt.Sprintf("The value for 3 with theta0 %.6f and theta1 %.6f is: %.6f", theta0, theta1, Hypothesis(theta0, theta1, 3)))
+}
